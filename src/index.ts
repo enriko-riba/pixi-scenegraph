ed951d36a4a8d2316b98a0dfbee91dfada97a447
enriko-riba <enriko.riba@gmail.com>
2019-05-27 21:22:07 +0200

    added missing exports

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export { DefaultScreenSizeCalculator } from './core/DefaultScreenSizeCalculator'
 //  scenegraph
 export { SceneManager } from './core/SceneManager';
 export { Scene } from './core/Scene';
-
+export { IController } from './core/IController';
+export { IRendererOptions } from './core/IRendererOptions';
 export { VERSION } from './_version';
